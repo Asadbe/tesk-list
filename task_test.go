0803ac04896ef444c932780f9d1b244bb494f902
Asadbe <asadbekbakhodirov2@gmail.com>
2020-02-18 21:46:05 +0500

    test

--- task_test.go
+++ task_test.go
@@ -1,35 +1,46 @@
 package main
+
 import (
 	"testing"
 	"time"
 )
+
+var (
+	cl ContactList
+)
+
 func TestAdd(t *testing.T) {
-	var i int
-	number := 5
-	cl := NewContactList()
-	cl.Add(&Contact{ID: number, Name: "write code", Assighec: "Doston", Deadline: time.Now().UTC().AddDate(0, 0, 15), Done: false})
-	cl.Add(&Contact{ID: 8, Name: "write code", Assighec: "Doston", Deadline: time.Now().UTC().AddDate(0, 0, 15), Done: true})
-	for i := 0; i < len(cl.contacts); i++ {
+	cl = NewContactList()
+	cl.Add(&Contact{ID: 0, Name: "write code", Assighec: "Doston", Deadline: time.Now().UTC().AddDate(0, 0, 15), Done: false})
+	cl.Add(&Contact{ID: 1, Name: "write code", Assighec: "Doston", Deadline: time.Now().UTC().AddDate(0, 0, 15), Done: true})
+	if len(cl.contacts) <= 0 {
+		t.Error("Error while adding contacts")
 	}
-	if cl.contacts[i].ID != number {
+
+	if cl.contacts[0].ID != 0 {
+		t.Error("Error while adding")
+	}
+}
+func TestUpdate(t *testing.T) {
+
+	i := 0
+	id := 0
+	name := "code"
+	assighec := "doni"
+	deadline := time.Now().UTC().AddDate(0, 0, -11)
+	done := true
+	cl.Update(0, &Contact{ID: id, Name: name, Assighec: assighec, Deadline: deadline, Done: done})
+
+	if cl.contacts[i].ID != id && cl.contacts[i].Name != name && cl.contacts[i].Assighec != assighec && cl.contacts[i].Deadline != deadline && cl.contacts[i].Done != done {
 		t.Errorf("the function is incorrect")
 	}
 
 }
-func TestUpdate(t *testing.T) {
-	var i int
-	 id:=8
-	 name:="code"     
-	 assighec:="doni"
-	 deadline:=time.Now().UTC().AddDate(0, 0, -11)
-	 done :=true
-	cl := NewContactList()
-	cl.Add(&Contact{ID: 100, Name: "write code", Assighec: "Doston", Deadline: time.Now().UTC().AddDate(0, 0, 15), Done: true})
-	cl.Update(100, &Contact{ID: id, Name: name, Assighec: assighec, Deadline: deadline, Done: done})
-	for i := 0; i < len(cl.contacts); i++ {
-	}
-	if cl.contacts[i].ID != id &&cl.contacts[i].Name != name&&cl.contacts[i].Assighec != assighec&&cl.contacts[i].Deadline != deadline&&cl.contacts[i].Done != done{
-		t.Errorf("the function is incorrect")
+func TestDelete(t *testing.T) {
+	cl.Delete(0)
+	if len(cl.contacts) != 1 {
+		t.Error("Error while adding contacts")
+
 	}
 
 }
